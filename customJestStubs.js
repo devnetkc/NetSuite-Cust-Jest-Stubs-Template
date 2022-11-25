da7aa9224f4562970e12d598318b25ce5e82bb95
Ryan Valizan <ryan@vnetkc.com>
2022-11-25 00:02:25 -0600

    ✨ entry file for pkg added

---
+++ customJestStubs.js
@@ -0,0 +1,28 @@
+/** @format */
+
+const CustomStubMap = {
+  'Modules/aModule': 'Modules/aModule',
+  'Modules/aModule.min':
+    'Modules/aModule' /*  Note we are not loading min for stubs */,
+  'Modules/bModule': 'Modules/bModule',
+  'Modules/bModule.min':
+    'Modules/bModule' /*  Note we are not loading min for stubs */,
+};
+
+const CUSTOM_STUBS = exclude =>
+  Object.keys(CustomStubMap).map(filename => {
+    const StubModuleName = `/SuiteScripts/${CustomStubMap[filename]}`;
+    let stubPath = `<rootDir>/node_modules/${PKG_STUBS_PATH}/${filename}.js`;
+    const Exclude = exclude.filter(
+      moduleObj => moduleObj.name === StubModuleName
+    );
+    if (Exclude.length > 0) stubPath = `${Exclude[0].path}${StubModuleName}`;
+    return {
+      module: StubModuleName,
+      path: stubPath,
+    };
+  });
+
+module.exports = {
+  customStubs: CUSTOM_STUBS,
+};
